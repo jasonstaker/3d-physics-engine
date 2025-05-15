e4dfc68551dd3221fdb3ade3ce9bf1bc85b7541e
jasonstaker <jason.staker@yahoo.com>
2025-05-15 16:48:28 -0700

    Added Renderer class skeleton

---
+++ include/Renderer.hpp
@@ -0,0 +1,29 @@
+#ifndef RENDERER_HPP
+#define RENDERER_HPP
+
+#include "SFML/Graphics.hpp"
+#include "Entity.hpp"
+#include "ShapeFactory.hpp"
+#include <vector>
+#include <memory>
+
+using namespace std;
+
+class Renderer {
+public:
+
+    // constructors
+    Renderer(unsigned int width, unsigned int height);
+    
+    // functions
+    void render(const vector<shared_ptr<Entity>>& entities);
+    void renderEntity(const shared_ptr<Entity>& entity);
+
+private:
+    unsigned int width; 
+    unsigned int height;
+    sf::RenderWindow* window;
+    ShapeFactory shapeFactory;
+};
+
+#endif
---
+++ src/main/ui/Renderer.cpp
@@ -0,0 +1,37 @@
+#include "Renderer.hpp"
+#include "Entity.hpp"
+#include "SFML/Graphics.hpp"
+#include <vector>
+#include <memory>
+#include <iostream>
+
+using namespace std;
+
+Renderer::Renderer(unsigned int width, unsigned int height) : width(width), height(height) {
+    window = new sf::RenderWindow(sf::VideoMode({width, height}), "Physics Engine");
+}
+
+void Renderer::render(const vector<shared_ptr<Entity>>& entities) {
+    
+    if (window->isOpen()) {
+        while (const std::optional event = window->pollEvent()) {
+            if (event->is<sf::Event::Closed>()) {
+                window->close();
+            } else if (const auto* keyPressed = event->getIf<sf::Event::KeyPressed>()) {
+                if (keyPressed->scancode == sf::Keyboard::Scancode::Escape) {
+                    window->close();
+                }
+            }
+        }
+
+
+        for (shared_ptr<Entity> entity : entities) {
+            renderEntity(entity);
+        }
+    }
+}
+
+void Renderer::renderEntity(const shared_ptr<Entity>& entity) {
+    auto drawnEntity = shapeFactory.
+    window->draw(drawnEntity);
+}
