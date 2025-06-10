c63330d75cbd6daff900151d9341d3e3cdd94103
jasonstaker <jason.staker@yahoo.com>
2025-06-10 14:50:04 -0700

    refactor: config refactor for global mutable simulation variables

--- include/Config.hpp
+++ include/Config.hpp
@@ -5,27 +5,32 @@
 namespace Config {
 
     // Window dimensions
-    static constexpr unsigned int windowWidth = 1500;
-    static constexpr unsigned int windowHeight = 1100;
+    constexpr unsigned int windowWidth = 1500;
+    constexpr unsigned int windowHeight = 1100;
 
     // Physics parameters
-    constexpr static float timeScale = 6.0f;
-    static Vec gravity = Vec(0, 9.81f);
-    constexpr static float dragCoefficient = 0.01f;
-    constexpr static float restitution = .8f;
-    constexpr static float frictionCoefficient = 1.0f;
+    constexpr float timeScale = 6.0f;
+    extern Vec gravity;
+    constexpr float dragCoefficient = 0.01f;
+    constexpr float restitution = .8f;
+    constexpr float frictionCoefficient = 1.0f;
 
     // Simulation variables
-    static constexpr float simulationRunTime = 6.0f;
-    static constexpr float renderFrequencyMultiplier = 3.0f;
-    static constexpr float targetFPS = 60.0f;
-    const double frameDuration = 1000.0 / targetFPS;
-    const float fixedTimeStep = 1.0f / (targetFPS*renderFrequencyMultiplier);
-    static const int numBalls = 500;
-    static const float radius = 20.0f;
-    static const float spawnMargin = radius;
-    static const int maxAttempts = 5000;
-    static const int qtSplitThreshold = 4;
-    static bool renderQT = false;
-    static bool showAABBs = false;
+    constexpr float simulationRunTime = 6.0f;
+    constexpr float renderFrequencyMultiplier = 3.0f;
+    constexpr float targetFPS = 60.0f;
+    extern const double frameDuration;
+    extern const float fixedTimeStep;
+    constexpr int numBalls = 500;
+    constexpr float radius = 20.0f;
+    constexpr float spawnMargin = radius;
+    constexpr int maxAttempts = 5000;
+    constexpr int qtSplitThreshold = 4;
+    
+    // Mutable simulation variables
+    extern bool renderQT;
+    extern bool showAABBs;
+    extern bool paused;
+    extern bool stepOnceRequested;
+    extern bool running;
 }
